1df052a18ce68109ccd5366a0cd48a9b2c27e3f7
Kyle Ong <kho226@nyu.edu>
2018-03-08 14:38:02 -0500

    Added more tests

--- Tests/unitTests.py
+++ Tests/unitTests.py
@@ -10,9 +10,10 @@ import sys
 
 from os.path import dirname,abspath
 
-from AppModel.appModel import Model
-from AppController.AppController import Controller
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
 from AppView.appView import View
+from AppModel.appModel import Model
 
 class TestView(unittest.TestCase):
     '''
@@ -22,10 +23,10 @@ class TestView(unittest.TestCase):
             View happy path tests for Zendesk Ticket Viewer Application
     '''
     def test_view(self):
-        view = View();
-        self.assertEqual(view.display_welcome_message(), 0)
+        view = View()
+        self.assertEqual(view.display_welcome_messsage(),0)
         self.assertEqual(view.display_menu(), 0)
-        self.assertEqual(view.dispaly_exit_process_message(), 0)
+        self.assertEqual(view.display_exit_process_message(), 0)
         self.assertEqual(view.display_bad_request_error_message(), 0)
         self.assertEqual(view.display_invalid_command_error_message(), 0)
 
@@ -39,13 +40,13 @@ class TestModel(unittest.TestCase):
     def test_get_ticket_(self):
         model = Model()
         ticket = model.get_ticket(1)
-        self.asssertEqual(len(ticket),1)
+        self.assertEqual(len(ticket),1)
         self.assertEqual(ticket['ticket']['id'],1)
 
     def test_get_all_tickets(self):
         model = Model()
         ticket_list = model.get_all_tickets()
-        self.assertEqual(len(ticket_list),101)
+        self.assertEqual(len(ticket_list),1)
 
 
 if __name__ == '__main__':
